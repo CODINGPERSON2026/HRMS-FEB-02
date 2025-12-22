6ab15dbaa589cbeff5cd89904c8e8d45ddf5cd20
CODER <codinperson126@gmail.com>
2025-12-22 15:08:32 +0530

    leaveupdate

--- blueprints/apply_leave.py
+++ blueprints/apply_leave.py
@@ -230,3 +230,71 @@ def get_leave_requests():
     finally:
         cursor.close()
         conn.close()
+
+
+@leave_bp.route("/get_leave_request/<int:leave_id>", methods=["GET"])
+def get_leave_request(leave_id):
+    conn = None
+    cursor = None
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        query = """
+            SELECT 
+                id,
+                army_number,
+                leave_type,
+                leave_days,
+                created_at,
+                leave_reason,
+                request_status
+            FROM leave_status_info
+            WHERE id = %s
+        """
+        cursor.execute(query, (leave_id,))
+        leave = cursor.fetchone()
+        print(leave_id)
+
+        if not leave:
+            return jsonify({
+                "success": False,
+                "message": "Leave request not found"
+            }), 404
+        query = ''' select name from personnel where army_number = %s'''
+        cursor.execute(query,(leave['army_number'],))
+        result_name =  cursor.fetchone()
+        leave['name'] = result_name['name']
+        
+        query = '''SELECT
+  CASE %s
+    WHEN   'AL'  THEN al_days
+    WHEN  'CL'  THEN cl_days
+    WHEN  'AAL' THEN aal_days
+  END AS leave_days
+FROM leave_details
+WHERE army_number = %s;
+'''      
+        print('fine till herer')
+        cursor.execute(query,(leave['leave_type'],leave['army_number']))
+        leave_days_left = cursor.fetchone()
+        leave['days_left'] = leave_days_left['leave_days']
+         
+        return jsonify({
+            "success": True,
+            "data": leave
+        })
+
+    except Exception as e:
+        print(str(e))
+        return jsonify({
+            "success": False,
+            "message": "Server error",
+            "error": str(e)
+        }), 500
+
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
